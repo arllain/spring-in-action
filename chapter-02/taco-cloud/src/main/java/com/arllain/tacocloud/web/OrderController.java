3ab98d3b09890a83024c916fbc4a9912bd23ceea
Arllain <arllain@gmail.com>
2024-04-14 08:09:19 -0300

    Validating form input

--- chapter-02/taco-cloud/src/main/java/com/arllain/tacocloud/web/OrderController.java
+++ chapter-02/taco-cloud/src/main/java/com/arllain/tacocloud/web/OrderController.java
@@ -1,13 +1,16 @@
 package com.arllain.tacocloud.web;
 
 import com.arllain.tacocloud.domain.tacos.TacoOrder;
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -15,15 +18,32 @@ import org.springframework.web.bind.support.SessionStatus;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
-    @GetMapping("/current")
-    public String orderForm(){
-        return "orderForm";
+  @GetMapping("/current")
+  public String orderForm() {
+    return "orderForm";
+  }
+
+/*
+  @PostMapping
+  public String processOrder(TacoOrder order,
+		  SessionStatus sessionStatus) {
+    log.info("Order submitted: {}", order);
+    sessionStatus.setComplete();
+
+    return "redirect:/";
+  }
+*/
+
+  @PostMapping
+  public String processOrder(@Valid TacoOrder order, Errors errors,
+                             SessionStatus sessionStatus) {
+    if (errors.hasErrors()) {
+      return "orderForm";
     }
 
-    @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
-        log.info("Order submitterd: {}", order);
-        sessionStatus.setComplete();
-        return "redirect:/";
-    }
+    log.info("Order submitted: {}", order);
+    sessionStatus.setComplete();
+
+    return "redirect:/";
+  }
 }
